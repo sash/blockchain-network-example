90c6f80fcf6b94bab8e989c00554aeee2609b67a
Alexander (SASh) Alexiev <alex@edge-soft.com>
2018-06-18 14:17:34 +0300

    added travis env

---
+++ tests/Unit/Node/BalanceTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sash
+ * Date: 18.6.2018
+ * Time: 14:15
+ */
+
+namespace Tests\Unit\Node;
+
+use App\Node\Balance;
+use PHPUnit\Framework\TestCase;
+
+class BalanceTest extends TestCase
+{
+    
+    public function testAddBlock()
+    {
+    
+    }
+    
+    public function testAddTransaction()
+    {
+    
+    }
+}
